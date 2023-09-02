e4bac3a7e8aebcc6dc7af4b9588d0aa3b91bc546
kanako777 <30712471+kanako777@users.noreply.github.com>
2023-09-02 20:15:43 +0900

    Add files via upload

--- cvx-test.py
+++ cvx-test.py
@@ -1,20 +1,32 @@
 import numpy as np
 import cvxpy as cvx
+from cvxpy import *
+import math
 
-f = np.array([[3],[3/2]])
-lb = np.array([[-1], [0]])
-ub = np.array([[2], [3]])
+# Define the variable
+x = cvx.Variable(pos=True)
+y = cvx.Variable(pos=True)
+z = cvx.Variable(pos=True)
 
-x = cvx.Variable([2,1])
+cost = square(x + inv_pos(y))
 
-obj = cvx.Minimize(-f.T@x)
-constraints = [lb <=x, x <= ub]
+# Define the objective function
+objective = cvx.Minimize(z)
 
-prob = cvx.Problem(obj, constraints)
-result = prob.solve()
+# Define the constraints
+constraints = [z>=cost] + [0<=x, x<=1] + [0<=y, y<=1]
 
-#print(x.value)
-#print(result)
+# Formulate the problem
+problem = cvx.Problem(objective, constraints)
+
+# Solve the problem
+problem.solve(gp=False)
+
+# Print the results
+print("Optimal value of x:", x.value)
+print("Optimal value of y:", y.value)
+print("Optimal value of z:", z.value)
+
 
 class Task:
     def __init__(self, id, frequency, size, cycle, delay):
@@ -24,105 +36,129 @@ class Task:
         self.cycle = cycle
         self.delay = delay
 
-
-#e_u_loc = e_u_m * task_size * task_frequency * ( X_U * f_u_k^2 + x_u_k * F_U^2 ) + 1/2 * ( (X_U - x_u_k)^2 + (F_U - f_u_k)^2 )
-#e_u_tx = P_u_b * X_B * task_size / R_u_b
-#t_u_loc = task_size * task_frequency * ( 1/2 * ((X_U + 1/F_U)^2 - (x_u_k)^2 - (1/f_u_k)^2) - (x_u_k * (X_U - x_u_k)) + (1/f_u_k)^3 * (1/F_U - 1/f_u_k))
-#t_b_off = task_size * task_frequency * ( 1/2 * ((X_B + 1/F_B)^2 - (x_b_k)^2 - (1/f_b_k)^2) - (x_b_k * (X_B - x_b_k)) + (1/f_b_k)^3 * (1/F_B - 1/f_b_k))
-#t_tx = X_B * task_size / R_u_b
-
-
 from cvxpy import *
 M = 4
 U = 1
-B = 3
-FU = 1
-FB = 2
+B = 4
+FU = 10
+FB = 20
 omega1 = 1
 omega2 = 1
 epsilon_u = 1
 
-xum = cvx.Variable([M, U])
-xbm = cvx.Variable([M, B])
-fum = cvx.Variable([M, U])
-fbm = cvx.Variable([M, B])
-sum_xbm = cvx.Variable([M, B])
-sum_fum = cvx.Variable([M, U])
-sum_fbm = cvx.Variable([M, B])
-mum = cvx.Variable(M)
+rho_um = cvx.Variable([M, U], pos=True)
+rho_bm = cvx.Variable([M, B], pos=True)
+fum = cvx.Variable([M, U], pos=True)
+fbm = cvx.Variable([M, B], pos=True)
+mum = cvx.Variable(M, pos=True)
 
-P_ub = np.ones((U, B)) * 0.1
-R_ub = np.ones((U, B)) * 50
-sm = np.ones(M) * 10
-fm = np.ones(M) * 10
 
-x_u_k = np.ones((M, U))
-x_b_k = np.ones((M, B))
-f_u_k = np.ones((M, U))
-f_b_k = np.ones((M, B))
+P_ub = np.ones((U, B)) * 1
+R_ub = np.ones((U, B)) * 1
+tou_rho_um = np.ones((M, U)) * 1
+tou_f_um = np.ones((M, U)) * 1
+sm = np.ones(M) * 10 # size of task m
+cm = np.ones(M) * 10 # cpu cycles required for task m
+dm = np.ones(M) * 10 # delay constraints for task m
+
+rho_um_k = np.ones((M, U)) * 0.2
+rho_bm_k = np.ones((M, B)) * 0.2
+f_u_k = np.ones((M, U)) * 2.5
+f_b_k = np.ones((M, B)) * 5
+mum_k = np.ones((M, U))
+#mum = np.ones((M, U))
 t_u_loc = np.zeros(M)
 
-cost1 = 0
-cost2 = 0
-#t_tx_cost = np.zeros((M, B))
-#t_off_cost = np.zeros((M, B))
-#t_tx = np.zeros(M)
-#t_off = np.zeros(M)
+difference = 2
+criteria = 1
+result_initial = 0
+loop = 1
 
-t_tx_cost = [[0 for _ in range(B)] for _ in range(M)]
-t_off_cost = [[0 for _ in range(B)] for _ in range(M)]
-t_tx = [0 for _ in range(M)]
-t_off = [0 for _ in range(M)]
+while (loop) :
+
+    loop = 1
+    initial_result = 0
+    stable = 2
+
+    e_um_cost = 0
+    e_tx_cost = 0
+    e_bm_cost = 0
+
+    t_um_cost = 0
+    t_tx_cost = 0
+    t_bm_cost = 0
+
+    P2_energy_cost = 0
+    P2_time_cost = 0
+
+    for m in range(M):
+
+        e_um_cost = 0
+        t_um_cost = 0
+
+        for u in range(U):
+
+            t_tx_cost = 0
+            e_tx_cost = 0
+            t_bm_cost = 0
+
+            for b in range(B):
+
+                ####### START (for_b) #########
+                # task m 처리를 위해 UAV -> bus b로 데이터를 보내는 데 걸리는 시간 (t_tx) 계산 : 식(9)
+                t_tx_cost += rho_bm[m, b] * sm[m] / R_ub[u, b]
+
+                # task m 처리를 위해 UAV -> bus b로 데이터를 보내는 데 필요한 에너지(e_tx) 계산 : 식(10)
+                e_tx_cost += rho_bm[m,b] * P_ub[u,b] * sm[m] / R_ub[u,b]
+
+                # bus b가 task m 처리를 위해 걸리는 시간 (t~_bm) 계산 : 식(21)
+                #t_bm_cost += cm[m] * (0.5 * ( power(rho_bm[m, b],2) + power(inv_pos(fbm[m, b]),2) - power(rho_bm_k[m, b],2) - power(inv_pos(f_b_k[m, b]),2) - (rho_bm_k[m, b] * (rho_bm[m, b] - rho_bm_k[m, b])) + (power(inv_pos(f_b_k[m, b]),3) * (inv_pos(fbm[m, b]) - inv_pos(f_b_k[m, b])))))
+                t_bm_cost += cm[m] * (0.5 * (power( rho_bm[m, b] + inv_pos(fbm[m, b]), 2) - power(rho_bm_k[m, b],2) - power(inv_pos(f_b_k[m, b]),2) - (rho_bm_k[m, b] * (rho_bm[m, b] - rho_bm_k[m, b])) + (power(inv_pos(f_b_k[m, b]),3) * (inv_pos(fbm[m, b]) - inv_pos(f_b_k[m, b])))))
+                ####### END (for_b) ###########
+
+            # UAV가 task m 처리를 위해 걸리는 시간 (t~_um) 계산 : 식(19)
+            t_um_cost += cm[m] * ( 0.5 * (power(rho_um[m,u] + inv_pos(fum[m,u]),2) - power(rho_um_k[m,u],2) - power(inv_pos(f_u_k[m,u]),2) - ( rho_um_k[m,u] * (rho_um[m, u] - rho_um_k[m,u]) ) + power(inv_pos(f_u_k[m,u]),3) * (inv_pos(fum[m, u]) - inv_pos(f_u_k[m,u]))))
+
+            # UAV가 task m 처리를 위해 필요한 에너지 (e~_um) 계산 : 식(16)
+            e_um_cost += epsilon_u * cm[m] * (( rho_um[m,u] * f_u_k[m,u]**2 + rho_um_k[m,u] * fum[m,u]**2 )) + ( 0.5 * tou_rho_um[m,u] * (rho_um[m,u] - rho_um_k[m,u])**2 ) + ( 0.5 * tou_f_um[m,u] * (fum[m,u] - f_u_k[m,u])**2 )
+
+        # mum[m] = t_bm_cost + t_tx_cost
+        #P2_time_cost += omega1 * (t_tx_cost + t_bm_cost)
+
+        P2_time_cost += omega1 * mum[m]
+        P2_energy_cost += omega2 * (e_um_cost + e_tx_cost)
+
+    sum_rho_bm = cvxpy.sum(rho_bm, axis=1, keepdims=True)
+
+    P2 = P2_time_cost + P2_energy_cost
+    obj = cvx.Minimize(P2)
+    constraints = \
+        [0 <= fum, cvx.sum(fum) <=FU] + \
+        [0 <= fbm, cvx.sum(fbm) <=FB] + \
+        [rho_um + sum_rho_bm == 1] + \
+        [0 <= rho_um, rho_um <= 1] + \
+        [0 <= rho_bm, rho_bm <= 1] + \
+        [mum <= dm] + \
+        [mum >= t_um_cost] + \
+        [mum >= t_bm_cost+t_tx_cost]
+
+    prob = cvx.Problem(obj, constraints)
+    result = prob.solve()
+
+    criteria = abs(result - initial_result)
+    initial_result = result
+
+    rho_um_k = rho_um.value
+    rho_bm_k = rho_bm.value
+    f_u_k = fum.value
+    f_b_k = fbm.value
+    mum_k = mum.value
+
+    print(rho_um.value)
+    print(rho_bm.value)
+    print(fum.value)
+    print(fbm.value)
+    print(mum.value)
+    print(result)
 
 
-for m in range(M):
-    for u in range(U):
-        for b in range(B):
-            cost1 += P_ub[u,b] * xbm[m,b] * sm[m] / R_ub[u,b]
-            t_tx_cost[m][b] = xbm[m,b] * sm[m] / R_ub[u,b]
-            t_off_cost[m][b] = sm[m] * fm[m] * (
-                        1 / 2 * ((xbm[m, b] + 1 / fbm[m, b]) ** 2 - (x_b_k[m, b]) ** 2 - (1 / f_b_k[m, b]) ** 2) - (
-                            x_b_k[m, b] * (xbm[m, b] - x_b_k[m, b])) + (1 / f_b_k[m, b]) ** 3 * (
-                                    1 / fbm[m, b] - 1 / f_b_k[m, b]))
-
-        e_u_loc = epsilon_u * sm[m] * fm[m] * (( xum[m,u] * f_u_k[m,u]**2 + x_u_k[m,u] * fum[m,u]**2 ) + 1/2 * ( (xum[m,u] - x_u_k[m,u])**2 + (fum[m,u] - f_u_k[m,u])**2 ))
-        cost1 += omega2 * (cost1 + e_u_loc)
-
-        #t_u_loc = sm[m] * fm[m] * ( 1/2 * ( (xum[m,u] + inv_pos(fum[m,u]))**2 - (x_u_k[m,u])**2 - (1/f_u_k[m,u])**2) - (x_u_k[m,u] * (inv_pos(xum[m, u]) - x_u_k[m,u])) + (1/f_u_k[m,u])**3 * (inv_pos(fum[m, u]) - 1/f_u_k[m,u]))
-
-
-    cost1 += omega1 * mum[m] + cost1
-    #cost1 += omega1 + cost1
-
-for m in range(M):
-    for u in range(U):
-        for b in range(B):
-            sum_xbm += xbm[m,b]
-            #sum_fum += fum[m,u]
-            #sum_fbm += fbm[m,b]
-            t_tx[m] += t_tx_cost[m][b]
-            # print(t_tx[m])
-            t_off[m] += t_off_cost[m][b]
-
-# print(t_tx)
-sum_xbm = cvxpy.sum(xbm, axis=1, keepdims=True)
-
-obj = cvx.Minimize(cost1)
-constraints = \
-    [0 <= xum, xum <= 1] + \
-    [0 <= xbm, xbm <= 1] + \
-    [xum + sum_xbm == 1] + \
-    [0 <= fum, cvx.sum(fum) <=FU] + \
-    [0 <= fbm, cvx.sum(fbm) <=FB] + \
-    [t_u_loc <= mum]  \
-    # [t_tx <= mum] \
-
-prob = cvx.Problem(obj, constraints)
-result = prob.solve()
-
-print(xum.value)
-print(xbm.value)
-print(fum.value)
-print(fbm.value)
-print(mum.value)
-print(result)
